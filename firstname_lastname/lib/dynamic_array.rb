ec9e0fe97d4a2dca9bbe86d110689db51472a8a3
Jeffrey Chuc <jeffreychuc@gmail.com>
2017-12-20 14:31:35 -0800

    add dynamic array pop/push

--- firstname_lastname/lib/dynamic_array.rb
+++ firstname_lastname/lib/dynamic_array.rb
@@ -4,23 +4,40 @@ class DynamicArray
   attr_reader :length
 
   def initialize
+    @length = 0
+    @capacity = 8
+    @store = StaticArray.new(@capacity)
   end
 
   # O(1)
   def [](index)
+    self.check_index(index)
+    @store[index]
   end
 
   # O(1)
   def []=(index, value)
+    self.check_index(index)
+    @store[index] = value
+    @store
   end
 
   # O(1)
   def pop
+    self.check_removal
+    @length -= 1
+    ret_val = @store[@length]
+    @store[@length] = nil
+    ret_val
   end
 
   # O(1) ammortized; O(n) worst case. Variable because of the possible
   # resize.
   def push(val)
+    self.resize! if @length == @capacity
+    @store[@length] = val
+    @length += 1
+    @store
   end
 
   # O(n): has to shift over all the elements.
