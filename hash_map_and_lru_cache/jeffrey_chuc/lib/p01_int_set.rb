fb1a9f4a80f9c54a3678d50b904772c18d27ab32
Jeffrey Chuc <jeffreychuc@gmail.com>
2017-12-25 11:31:41 -0800

    add int_set

--- hash_map_and_lru_cache/jeffrey_chuc/lib/p01_int_set.rb
+++ hash_map_and_lru_cache/jeffrey_chuc/lib/p01_int_set.rb
@@ -1,14 +1,22 @@
 class MaxIntSet
   def initialize(max)
+    @store = Array.new(max) { false }
+    @length = max
   end
 
   def insert(num)
+    raise 'Out of bounds' if !validate!(num)
+    @store[num] = true
   end
 
   def remove(num)
+    raise 'Out of bounds' if !validate!(num)
+    @store[num] = false
   end
 
   def include?(num)
+    raise 'Out of bounds' if !validate!(num)
+    @store[num]
   end
 
   private
@@ -17,6 +25,7 @@ class MaxIntSet
   end
 
   def validate!(num)
+    num >= 0 && num < @length
   end
 end
 
@@ -27,12 +36,15 @@ class IntSet
   end
 
   def insert(num)
+    @store[num % num_buckets].push(num)
   end
 
   def remove(num)
+    @store[num % num_buckets].delete(num)
   end
 
   def include?(num)
+    @store[num % num_buckets].include?(num)
   end
 
   private
@@ -52,15 +64,23 @@ class ResizingIntSet
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
+    @prev_num_buckets = num_buckets
   end
 
   def insert(num)
+    if !self.include?(num)
+      @store[num % num_buckets].push(num)
+      @count += 1
+      resize! if @count == num_buckets
+    end
   end
 
   def remove(num)
+    @store[num % num_buckets].delete(num)
   end
 
   def include?(num)
+    @store[num % num_buckets].include?(num)
   end
 
   private
@@ -74,5 +94,11 @@ class ResizingIntSet
   end
 
   def resize!
+    old_store = @store
+    @store = Array.new(@prev_num_buckets * 2) { Array.new }
+    @prev_num_buckets = @prev_num_buckets * 2
+    # setting count to 0 because .insert changes count anyways.
+    @count = 0
+    old_store.flatten.each { |num| self.insert(num) }
   end
 end
