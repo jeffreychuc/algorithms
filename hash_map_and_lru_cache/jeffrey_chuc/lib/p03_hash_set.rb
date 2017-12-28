cde68f640d5eb8244e3da5f2649ae8dd3257abe1
Jeffrey Chuc <jeffreychuc@gmail.com>
2017-12-27 23:08:31 -0800

    add hash_set

--- hash_map_and_lru_cache/jeffrey_chuc/lib/p03_hash_set.rb
+++ hash_map_and_lru_cache/jeffrey_chuc/lib/p03_hash_set.rb
@@ -6,21 +6,28 @@ class HashSet
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
+    @prev_num_buckets = num_buckets
   end
 
   def insert(key)
+    self[key].push(key)
+    @count += 1
+    resize! if @count == num_buckets
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    self[key].delete(key)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +35,11 @@ class HashSet
   end
 
   def resize!
+    old_store = @store
+    @store = Array.new(@prev_num_buckets * 2) { Array.new }
+    @prev_num_buckets = @prev_num_buckets * 2
+    # setting count to 0 because .insert changes count anyways.
+    @count = 0
+    old_store.flatten.each { |num| self.insert(num) }
   end
 end
