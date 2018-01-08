11871ae138181ec9ac9dfd122622b1d15bbeb8fd
Jeffrey Chuc <jeffreychuc@gmail.com>
2018-01-08 10:51:58 -0800

    add kth largest

--- binary_search_tree/jeffrey_chuc/lib/bst_practical.rb
+++ binary_search_tree/jeffrey_chuc/lib/bst_practical.rb
@@ -1,3 +1,7 @@
+require_relative 'binary_search_tree'
 
 def kth_largest(tree_node, k)
+  new_tree = BinarySearchTree.new
+  new_tree.root = tree_node
+  new_tree.find(new_tree.in_order_traversal[-k])
 end
